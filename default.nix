e4c57fc40f0dc91a22546deb5afe953bbd4889da
Ian MacGuigan <ian.macguigan@gmail.com>
2020-02-23 15:34:40 -0500

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,13 @@
-{ mkDerivation, aeson, attoparsec, base, bytestring, containers
-, mtl, scientific, semigroupoids, stdenv, text
-, unordered-containers, vector
+{ mkDerivation, aeson, attoparsec, base, containers, scientific
+, stdenv, text, unordered-containers, vector
 }:
 mkDerivation {
   pname = "aeson-zippy";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson attoparsec base bytestring containers mtl scientific
-    semigroupoids text unordered-containers vector
+    aeson attoparsec base containers scientific text
+    unordered-containers vector
   ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
